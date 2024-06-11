eb8fa4a12e9f5c1a9cc98e2aaa831755fafa799b
안정인 <ttingin99@gmail.com>
2024-06-12 08:51:18 +0900

    단어의 개수 수정

--- src/BOJ/Bronze/js/BB1152_wordsQty.js
+++ src/BOJ/Bronze/js/BB1152_wordsQty.js
@@ -1,3 +1,3 @@
-let a = require('fs').readFileSync('../example/단어의 개수 예제.txt').toString().trim().split('\n');
+let a = require('fs').readFileSync('../example/단어의 개수 예제.txt').toString().trim().split(' ');
 
-console.log(a);
+console.log(a.length);
