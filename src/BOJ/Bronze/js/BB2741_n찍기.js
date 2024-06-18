b392b83a19e330fe6c4b468f3a578bdd96072b9a
안정인 <ttingin99@gmail.com>
2024-06-18 15:40:02 +0900

    n찍기 완성

--- src/BOJ/Bronze/js/BB2741_n찍기.js
+++ src/BOJ/Bronze/js/BB2741_n찍기.js
@@ -1,5 +1,7 @@
 let a = Number(require('fs').readFileSync('../example/n찍기예제.txt').toString().trim().split(' '));
-
+let text='';
 for (let i = 1; i <= a; i++) {
-    console.log(i);
+    text+=(i+'\n');
 }
+
+console.log(text)
