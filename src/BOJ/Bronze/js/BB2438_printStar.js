5b17d94c3d845bf06b24dffc38b2a697b77467e4
안정인 <ttingin99@gmail.com>
2024-06-18 14:35:35 +0900

    별찍기 2 완성

--- src/BOJ/Bronze/js/BB2438_printStar.js
+++ src/BOJ/Bronze/js/BB2438_printStar.js
@@ -2,8 +2,12 @@ let a = Number(require('fs').readFileSync('../example/별찍기1예제.txt').toS
 
 for (let i = 1; i <= a; i++) {
     let star='';
-    for (let j = 0; j < i; j++) {
-        star+='*';
+    for (let j = 1 ; j <= a; j++) {
+        if (a-(i-1)>j){
+            star+=' ';
+        } else {
+            star+='*';
+        }
     }
     console.log(star);
 }
